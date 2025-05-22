11b0eb7980c7f9af0d85d2d0312ecafe2563d92b
miguel-sccp <miguel.reis@hotmail.com.br>
2025-05-22 17:40:59 -0300

    Add files via upload

---
+++ calcularoda python.py
@@ -0,0 +1,8 @@
+#soma de 2
+n1 = float(input("numnero 1 :"))
+n2 = float(input("numnero 2 :"))
+calculo = n1 + n2
+print("soma:", n1+n2)
+print("subtração:", n1-n2)
+print("multiplicação:", n1*n2)
+print("divisão:", n1/n2)
