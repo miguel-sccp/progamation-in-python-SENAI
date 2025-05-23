7639c44860fee5352bacef23b6d14a721a877ba3
miguel-sccp <miguel.reis@hotmail.com.br>
2025-05-23 17:51:08 -0300

    Add files via upload

---
+++ atividades.py
@@ -0,0 +1,34 @@
+#bem vindo
+#1
+print("seja vem vindo")
+#2
+verdadeiro= True
+print(type(verdadeiro))
+#3
+print(10.5*2.00)
+#4
+print(10/2)
+#5
+print(10-5)
+#6
+print(10//2)
+#7
+print(5.4*2.2*4.6*3.1)
+#8
+numero = 10
+print(numero*2)
+#9
+nome=input("digite seu nome")
+idade=int(input("digite sua idade"))
+email=input("digite seu endereço de email")
+endereço=input("digite seu endereço")
+curso=input("digite seu curso")
+
+print(f'''
+
+nome:{nome}
+idade:{idade}
+email:{email}
+endereço:{endereço}
+curso:{curso}
+''')
