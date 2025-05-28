988e4704b61d35b6ab0b13c95fbcbedd45cafb0f
Miguel Reis Ribeiro de Almeida <miguel.reis@hotmail.com.br>
2025-05-28 17:52:09 -0300

    Add files via upload

---
+++ aula7.py
@@ -0,0 +1,20 @@
+#0
+print (list(range(2,22,2)))
+#1
+numeros=[0,1,2,3,4,5,6,7,8,9,10]
+print(numeros)
+#2
+print(numeros[2])
+#3
+numeros.append(9)
+print(numeros)
+#4
+del numeros[5]
+print(numeros)
+#5
+Carros=['Nivus','Tcross','Jetta']
+print(numeros,Carros)
+
+
+
+
