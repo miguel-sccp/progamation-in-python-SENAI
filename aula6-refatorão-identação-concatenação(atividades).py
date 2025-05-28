b24d23a369bd071b0006203f594d4258c027fe80
Miguel Reis Ribeiro de Almeida <miguel.reis@hotmail.com.br>
2025-05-28 16:54:24 -0300

    Add files via upload

---
+++ aula6-refatorão-identação-concatenação(atividades).py
@@ -0,0 +1,84 @@
+#1
+n1=4
+n2=6
+resultado= (n1*n2)/2
+print("o resultado é:", resultado)
+#2
+n3=5
+n4=7
+resultado= n3+n4
+print("o resultado é:", resultado)
+#3
+n5=3
+n6=4
+resultado =n5**n6
+print("o resultado é:", resultado)
+#4
+n7=8
+n8=12
+n9=15
+resultado=n7+n8+n9//3
+print("o resultado é:", resultado)
+#5
+n10=10
+n11=2
+n12=5
+resultado=(n10-n11)*n12
+print("o resultado é:", resultado)
+#6
+n13=27
+n14=3
+n15=5
+resultado=(n13/n14)+n15
+print("o resultado é:", resultado)
+#7
+n16=17
+n17=4
+resultado=(n16 % n17)
+print("o resultado é:", resultado)
+#8
+n18=9
+n19=3
+resultado=(n18*n19)**2
+print("o resultado é:", resultado)
+#9
+n20=20
+n21=3
+n22=4
+resultado=(n20+n21)*n22
+print("o resultado é:", resultado)
+#10
+n23=15
+n24=2
+n25=7
+resultado= (n23*n24)-n25
+print("o resultado é:", resultado)
+#11
+n26=5
+n27=3
+resultado=(n26**n27)
+print("o resultado é:", resultado)
+#12
+n28=17
+n29=21
+n30=25
+resultado=(n28+n29+n30)/3
+print("o resultado é:", resultado)
+#13
+n31=11
+n32=2
+n33=7
+resultado=(n31*n32)+n33
+print("o resultado é:", resultado)
+#14
+n34=15
+n35=3
+n36=8
+n37=2
+resultado=((n35*n36)-n34)/n37
+print("o resultado é:", resultado)
+#15
+n38=2
+n39=10
+resultado=n38**n39
+print("o resultado é:", resultado)
