a29b55457ac877738e019b1553a5c935f9babe5f
Miguel Reis Ribeiro de Almeida <miguel.reis@hotmail.com.br>
2025-05-30 16:57:30 -0300

    Add files via upload

---
+++ atividade_8.py
@@ -0,0 +1,50 @@
+#1
+numero=int(input("digite um numero ai "))
+if numero > 0 :
+    print('seu numero e positivo')
+
+else:
+    print('seu numero é negativo')
+#2
+idade=int(input('digite sua idade'))
+if idade>= 16:
+ print("vc pode votar")
+if idade< 16:
+ print("vc nn pode votar")
+#3
+numero=int(input('digite um numero ai'))
+if numero % 2==0:
+ print ('par')
+else:
+    print('impar')
+#4
+n1=input('digite o 1º numero')
+n2=input('digite o 2º numero')
+n3=input('digite o 3º numero')
+
+if n1==n2==n3==n1:
+  print('é um triangulo equilátero')
+elif n1!=n2!=n3!=n1:
+ print('é um triangulo escaleno')
+else:
+ print('é um triangulo isóceles')
+ #5
+n1=int(input('escreva um numero'))
+if n1%7==0 or n1%5==0:
+  print ( 'seu numero e multiplo de 7 e 5')
+else:
+    print('seu numero não e multiplo de 7 e 5')
+#6
+n1=int(input('escreve um numero ai'))
+if n1> 10:
+ print('seu numero e maior q 10')
+else:
+  print('seu numero e menor q 10')
+#7
+n1=int(input('digite mais um nuemro ai cara'))
+if n1%3==0 or n1%5==0:
+   print('seu numero é divisivel por 3, 5')
+else:
+    print(' seu nuemro n é divisivel por 3 , 5')
+
+ 
